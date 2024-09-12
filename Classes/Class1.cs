763579d12b11aad85b7867e7c4920206ff5af750
Robert <robcc1527@gmail.com>
2024-09-12 15:46:45 -0500

    working on Launch Code

---
+++ Classes/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Course
+{
+    private Dictionary<int, int> gradebook { get; set; }
+    private DateTime classTime { get; set; }
+    private string teacher { get; set; }
+
+
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,1 +1,12 @@
 ﻿// TODO: Instantiate your objects and test your exercise solutions with print statements here.
+
+namespace Classes;
+class Program
+{ 
+
+     static void Main(String[] args)
+    {
+        Student Robert = new Student("Robert", 1, 1, 4.0);
+        Console.WriteLine("The Student class works! " + Robert.Name + " is a student!");
+    }
+}
